297349e5a7462ef770743e486b98532f5280e008
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-15 00:01:30 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/metavision/accurate-drone-shapessegmentation"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 1644750
+PREVIEW_IMAGE_ID: int = 1636871
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/accurate-drone"
